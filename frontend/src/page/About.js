ef197dc73619b831b678f2c5ad9a601eb90c4d1a
Akhil@LAPTOP-G7648CH4 <deshmukhakhil66@gmail.com>
2023-06-01 19:22:35 +0530

    MedStore

---
+++ frontend/src/page/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='bg-cyan-50'>
+    <div className='text-rose-500 text-4xl '>
+    Medatore is a leading healthcare technology company specializing in medical data management and analytics. Our mission is to revolutionize the way medical information is handled and utilized to enhance patient care and improve healthcare outcomes.
+Founded in 2010, Medatore has quickly established itself as a trusted partner for healthcare providers, pharmaceutical companies, and research institutions worldwide. Our team of experienced professionals combines expertise in healthcare, data science, and technology to deliver innovative solutions that address the complex challenges of the healthcare industry.
+At Medatore, we are committed to driving innovation and transforming healthcare through the power of data. Our goal is to empower healthcare professionals, researchers, and patients with actionable insights that lead to better healthcare delivery, improved patient outcomes, and ultimately, a healthier future for all.
+    </div>
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/page/Home.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import CardFeature from "../component/CardFeature";
+import HomeCard from "../component/HomeCard";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import FilterProduct from "../component/FilterProduct";
+import AllProduct from "../component/AllProduct";
+
+
+const Home = () => {
+  const productData = useSelector((state) => state.product.productList);
+  const homeProductCartList = productData.slice(0, 4);
+  const homeProductCartListVitamins = productData.filter(
+    (el) => el.category === "vitamins",
+    []
+  );
+  const loadingArray = new Array(4).fill(null);
+  const loadingArrayFeature = new Array(10).fill(null);
+
+  const slideProductRef = useRef();
+  const nextProduct = () => {
+    slideProductRef.current.scrollLeft += 200;
+  };
+  const preveProduct = () => {
+    slideProductRef.current.scrollLeft -= 200;
+  };
+
+
+ 
+
+
+  
+
+
+
+  return (
+    <div className="p-2 md:p-4 bg-cyan-50">
+      <div className="md:flex gap-4 py-2">
+        <div className="md:w-1/2">
+          <div className="flex gap-3 bg-slate-300 w-36 px-2 items-center rounded-full">
+            <p className="text-sm font-medium text-slate-900">Instant Delivery</p>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2972/2972185.png"
+              className="h-7"
+            />
+          </div>
+          <h2 className="text-4xl md:text-7xl font-bold text-rose-500 py-3">Your Trusted Pharmacy Partner</h2>
+          <p className="py-3  text-rose-900 text-lg">
+          At MedStore, we believe that taking care of your health should be easy and tailored to your needs. That's why we've created an app that puts you in control. With MedStore, you have the power to manage your healthcare journey - from browsing a wide range of healthcare products to tracking your orders and accessing helpful health resources. Enjoy a personalized and empowering healthcare experience with MedStore.
+          </p>
+          <button className="font-bold bg-red-500 hover:bg-lime-500 text-slate-200 px-4 py-2 rounded-full">
+            Order Now
+          </button>
+        </div>
+
+        <div className="md:w-1/2 flex flex-wrap gap-5 p-4 justify-center">
+          {homeProductCartList[0]
+            ? homeProductCartList.map((el) => {
+                return (
+                  <HomeCard
+                    key={el._id}
+                    id={el._id}
+                    image={el.image}
+                    name={el.name}
+                    price={el.price}
+                    category={el.category}
+                  />
+                );
+              })
+            : loadingArray.map((el, index) => {
+                return <HomeCard key={index+"loading"} loading={"Loading..."} />;
+              })}
+        </div>
+      </div>
+
+      <div className="">
+        <div className="flex w-full items-center">
+          <h2 className="font-bold text-2xl text-slate-800 mb-4">
+            Vitamins Tablets
+          </h2>
+          <div className="ml-auto flex gap-4">
+            <button
+              onClick={preveProduct}
+              className="bg-slate-300 hover:bg-slate-400 text-lg  p-1 rounded"
+            >
+              <GrPrevious />
+            </button>
+            <button
+              onClick={nextProduct}
+              className="bg-slate-300 hover:bg-slate-400 text-lg p-1 rounded "
+            >
+              <GrNext />
+            </button>
+          </div>
+        </div>
+        <div
+          className="flex gap-5 overflow-scroll scrollbar-none scroll-smooth transition-all"
+          ref={slideProductRef}
+        >
+          {homeProductCartListVitamins[0]
+            ? homeProductCartListVitamins.map((el) => {
+                return (
+                  <CardFeature
+                    key={el._id+"vitamins"}
+                    id={el._id}
+                    name={el.name}
+                    category={el.category}
+                    price={el.price}
+                    image={el.image}
+                  />
+                );
+              })
+            : loadingArrayFeature.map((el,index) => (
+                <CardFeature loading="Loading..." key={index+"cartLoading"} />
+              ))}
+        </div>
+      </div>
+      
+      <AllProduct heading={"Your Product"}/>
+    </div>
+  );
+};
+
+export default Home;
